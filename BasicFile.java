7d0d8601246d112703418e7aa14da0393a5c4e91
Will <wcisco17@Wills-iMac.local>
2019-01-17 23:20:32 +0700

    Added Read

---
+++ BasicFile.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;
+
+public class BasicFile {
+    public static void main(String[] args) {
+        File file = new File("test.txt");
+
+        // Write to File
+        try {
+            PrintWriter output = new PrintWriter(file);
+            output.println("Williams Sissoko");
+            output.println(23);
+            output.close();
+        } catch (IOException ex) {
+            System.out.printf("Error: %s\n", ex);
+        }
+
+
+        // Read to file
+        try {
+            Scanner in = new Scanner(file);
+            String name = in.nextLine();
+            int age = in.nextInt();
+            in.close();
+        
+            System.out.printf("Name: %s Age: %d\n", name, age);
+
+        } catch (FileNotFoundException ex) {
+            System.out.printf("Error: %s\n", ex);
+        }
+    }
+}
+
+
+
+
+
